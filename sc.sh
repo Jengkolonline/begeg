e1e1066fd0b110d39646c7e1883d9c796efa46bb
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-07-10 03:16:00 +0700

    Create sc.sh

---
+++ sc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;33m Secen Bug & Crot Crot \033[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p "Masukan Domain Bug: " domain
+
+./begeg $domain
