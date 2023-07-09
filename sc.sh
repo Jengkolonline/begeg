511a030ebe17062080d843ca18cf516fe34d7584
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-07-10 04:45:00 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 ipsaya=$(wget -qO- ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
