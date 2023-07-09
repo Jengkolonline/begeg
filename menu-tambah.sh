2ba16f958337f6f1772ded79ef817ce26f688174
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-07-10 02:32:38 +0700

    Update menu-tambah.sh

--- menu-tambah.sh
+++ menu-tambah.sh
@@ -32,7 +32,6 @@ echo -e " [\e[36m•1\e[0m] Secend Domain/IP Bug Websocket "
 echo -e " [\e[36m•2\e[0m] Cari Bug "
 echo -e " [\e[36m•3\e[0m] Test Speed Global "
 echo -e " [\e[36m•4\e[0m] Install DNS Global Nonton "
-echo -e " [\e[36m•5\e[0m] Check User Login Vmess "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
@@ -47,7 +46,6 @@ case $opt in
 2) clear ; cari-bug ; exit ;;
 3) clear ; global ; exit ;;
 4) clear ; nf ; exit ;;
-5) clear ; cek-ws ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; m-sshovpn ;;
