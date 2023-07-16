e149509039135128700796bb0b96a7a26f0be654
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-07-17 01:49:39 +0700

    Update menu-tambah.sh

--- menu-tambah.sh
+++ menu-tambah.sh
@@ -24,20 +24,21 @@ checking_sc() {
 }
 checking_sc
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m       • PENGATURAN TAMBAHAN MENU •         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-#echo -e " [\e[36m•1\e[0m] Secend Domain/IP Bug Websocket "
-echo -e " [\e[36m•1\e[0m] Cari Bug "
-echo -e " [\e[36m•2\e[0m] Test Speed Global "
-echo -e " [\e[36m•3\e[0m] Install DNS Global Nonton "
-echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
+echo -e " \033[31m╭══════════════════════════════════════════════════════════╮\e[0m"
+echo -e " \033[34m│$NC\033[33m                       MENU Sistem                        $NC\033[34m│\e[0m"
+echo -e " \033[33m╰══════════════════════════════════════════════════════════╯\e[0m"
+echo -e " \033[32m╭══════════════════════════════════════════════════════════╮\e[0m"
+echo -e " \033[35m│$NC [01]${NC} \033[0;36m Cari Bug Clondflare${NC}"
+echo -e " \033[35m│$NC [02]${NC} \033[0;36m Monitor & Speed Test Global${NC}"
+echo -e " \033[35m│$NC [03]${NC} \033[0;36m Install DNS Netflix,Hostar,YT,TikTok,iQiyi,Viu,Strem${NC}"
+echo -e " \033[35m│$NC"
+echo -e " \033[35m│$NC [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e " \033[35m│$NC"
+echo -e " \033[35m│$NC Press x or [ Ctrl+C ] • To-Exit"
+echo -e " \033[35m│$NC"
+echo -e " \033[36m╰══════════════════════════════════════════════════════════╯\e[0m"
+echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
 echo -e ""
 read -p " Select menu :  "  opt
 echo -e ""
@@ -48,5 +49,5 @@ case $opt in
 3) clear ; nf ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
-*) echo "Anda salah tekan " ; sleep 1 ; m-sshovpn ;;
+*) echo "Anda salah tekan " ; sleep 1 ; menu ;;
 esac
